1cbb353c9c4fa6598320e255a3479b0a654c7ec0
utkarsh032 <utkarshraj525@gmail.com>
2023-12-17 00:54:20 +0530

    Hero Page UI designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 
 function App() {
 
   return (
     <Router>
-      <Navbar />
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
     </Router>
   )
 }
